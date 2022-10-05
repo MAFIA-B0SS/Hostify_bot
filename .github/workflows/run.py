728c3a2e6d3b43f1de9f37666de262adf27b3575
Mafia <39020530+MAFIA-B0SS@users.noreply.github.com>
2022-10-05 11:56:17 +0200

    Update run.py

--- .github/workflows/run.py
+++ .github/workflows/run.py
@@ -10,7 +10,7 @@ from html2image import Html2Image
 from html.parser import HTMLParser
 newCommands = 0
 BOT_TOKEN = os.environ.get("CODER_NINJA")
-bot = telebot.TeleBot("5584331715:AAGQe97LZP5J2hwfDa-b4xj3VRjh8U_pquo")
+bot = telebot.TeleBot(BOT_TOKEN)
 def PostRandom(Lan=""):
     groupsIds = {"python":"-1001567978602","php":"-699912702","pytelegrambotapi":"-1001540211939"}
     
